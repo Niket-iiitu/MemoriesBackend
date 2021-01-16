43d5bd36c5b7507f59be97dd6e12da2e652e52ae
Niket-iiitu <niketagrawal777@gmail.com>
2021-01-16 17:12:25 +0530

    updates

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import router from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/posts", router);
 
-const CONNECTION_URL = `mongodb+srv://niketagrawal:agrawal973@cluster0.vvpvd.mongodb.net/niketagrawal?retryWrites=true&w=majority`;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
